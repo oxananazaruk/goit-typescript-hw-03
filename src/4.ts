b04086dfe6b72d0145efbb6cdb11a7c1dcd26e9d
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2024-01-01 19:01:15 +0200

    task 3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,20 @@
+class Key {
+    constructor(private signature: number) {
+        this.signature = Math.random();
+    }
+
+    getSignature() {
+        return this.signature
+    }
+}
+
+class Person {
+    constructor() {}
+}
+
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
